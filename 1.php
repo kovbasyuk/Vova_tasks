269f8f5987a06ecc1afb595d32316696dc18f393
kovbasyuk <kovbasyuk_i@tdmu.edu.ua>
2015-03-18 16:13:58 +0200

    new 1

---
+++ 1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+</head>
+<body>
+<div>
+
+    <?php
+    $name = "Ігор";
+    $age = 24;
+
+    echo "Моє ім'я". $name. "<br>Мені". $age. " років.";
+    ?>
+</div>
+</body>
+</html>
