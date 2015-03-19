667723bbadd88d5907672519502da80d9a2b30fc
kovbasyuk <kovbasyuk_i@tdmu.edu.ua>
2015-03-19 16:04:55 +0200

    Конструкція Switch - Case.

--- 1.php
+++ 1.php
@@ -7,20 +7,22 @@
 <div>
 
     <?php
-    $name = "Ігор";
-    $age = 24;
-    if($age >=18 && $age <=59){
-        echo "Вам ще працювати і працювати";
+    $day=10;
+    switch($day){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            echo "Це робочий день";
+            break;
+        case 6:
+        case 7:
+            echo "Це вихідний день";
+            break;
+        default:
+            echo "Не відомий день";
     }
-    elseif($age > 59){
-        echo "Вам пора на пенсію";
-    }
-    else{
-        if($age >=1 && $age<18){
-            echo "Вам ще зарано працювати";
-        }else{echo "Не відомий вік";}}
-
-
     ?>
 
 </div>
