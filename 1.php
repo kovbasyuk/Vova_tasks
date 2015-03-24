25ddc748b541787ea5ed252a8134ed2383b46b8b
kovbasyuk <kovbasyuk_i@tdmu.edu.ua>
2015-03-24 16:34:40 +0200

    Цикл For

--- 1.php
+++ 1.php
@@ -7,12 +7,10 @@
 <div>
 
     <?php
-    $bmw = array("model" => "X5", "speed" => "120", "doors" => "5", "year" => "2006");
-    $toyota = array("model" => "Carina", "speed" => "130", "doors" => "4", "year" => "2007");
-    $opel = array("model" => "Corsa", "speed" => "140", "doors" => "5", "year" => "2007");
-    echo " Модель-". $bmw ["model"].", Швидкість-".$bmw ["speed"].", Дверок-".$bmw ["doors"].", Рік-".$bmw ["year"]."<br>";
-    echo " Модель-". $toyota ["model"].", Швидкість-".$toyota ["speed"].", Дверок-".$toyota ["doors"].", Рік-".$toyota ["year"]."<br>";
-    echo " Модель-". $opel ["model"].", Швидкість-".$opel ["speed"].", Дверок-".$opel ["doors"].", Рік-".$opel ["year"]."<br>";
+    $plus = 2;
+    for ($start = 1; $start < 50; $start += $plus){
+        echo $start."-";
+    }
     ?>
 
 </div>
